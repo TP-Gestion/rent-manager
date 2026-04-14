d7a275c09105b80b26fe8bc8add73fc7fa30443b
jcontegrand <jcontegrand@americavirtualsa.com>
2026-04-14 09:17:42 -0300

    Test pipeline

--- backend/src/main/java/ar/com/aeb/alquileres/controller/PingController.java
+++ backend/src/main/java/ar/com/aeb/alquileres/controller/PingController.java
@@ -21,7 +21,7 @@ public class PingController {
         response.put("status", "UP");
         response.put("message", "Alquileres API is running");
         response.put("timestamp", LocalDateTime.now());
-        response.put("version", "1.0");
+        response.put("version", "1.1");
         
         return ResponseEntity.ok(response);
     }
