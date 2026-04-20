0f3fdf8473166f74c392a25aa3b1fbc69653736f
Nicolas <nfunes@fi.uba.ar>
2026-04-20 01:06:50 -0300

    fix: cors

--- backend/src/main/java/ar/com/aeb/alquileres/config/CorsConfig.java
+++ backend/src/main/java/ar/com/aeb/alquileres/config/CorsConfig.java
@@ -1,6 +1,6 @@
 package ar.com.aeb.alquileres.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
